5e4e206760c5e02b9e37b9956f7e8ea7288eec6f
Artur Mamedov <arturmamedov1993@gmail.com>
2026-02-23 17:18:11 +0100

    Products fetched from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  function apiCallProducts() {
+    fetch('https://fakestoreapi.com/products')
+      .then(function (result) { return result.json() })
+      .then(products => {
+        console.log(products)
+        setProducts(products);
+      })
+  }
+
+  useEffect(apiCallProducts, []);
+
 
   return (
     <>
-      <p>Hello!</p>
+      <ul>
+        {products.map((product) => {
+          return (
+            <li key={product.id}>{product.title}</li>
+          )
+        })}
+      </ul>
     </>
   )
 }
