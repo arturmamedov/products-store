da52338058df8cedca56d36e8c12d55f0f9c6574
Artur Mamedov <arturmamedov1993@gmail.com>
2026-02-23 18:29:33 +0100

    Added - Search product functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,46 @@ import './App.css'
 import ProductCard from './components/ProductCard';
 
 function App() {
-  let [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  // 1 - leggere onChange, 2 - variabile di stato per la value, 3 - filtro dei dati
+  const [searchProduct, setSearchProduct] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
-  console.log('app')
 
   function apiCallProducts() {
-    console.log('useEffect')
-
     fetch('https://fakestoreapi.com/products')
       .then(function (result) { return result.json() })
       .then(data => {
-        console.log(data)
         setProducts(data);
+        setFilteredProducts(data);
       })
   }
-
   useEffect(apiCallProducts, []);
 
+  function onSearchForm(e) {
+    e.preventDefault();
+    console.log('on search')
+
+    const filteredProducts = products.filter(product => product.title.toLowerCase().includes(searchProduct.toLowerCase()));
+    setFilteredProducts(filteredProducts)
+  }
+
+  const resultProducts = filteredProducts ? true : false;
+  console.log('result', resultProducts)
 
   return (
     <>
-      {products.map((product) => <ProductCard product={product} />)}
+      <form className="search-bar" onSubmit={onSearchForm}>
+        <input
+          type="text"
+          placeholder="Cerca prodotti..."
+          value={searchProduct}
+          onChange={(e) => setSearchProduct(e.target.value)}
+        />
+        <input type="submit" value="Cerca" />
+      </form>
+
+      {filteredProducts.length == 0 ? filteredProducts.map((product) => <ProductCard product={product} />) : 'Nessun prodotto trovato'}
     </>
   )
 }
