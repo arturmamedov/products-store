b845c0f469593f1b57967c651824f9504ba2d41c
Stefano Cappellini <sc.bool@stefanocappellini.com>
2026-03-19 16:56:33 +0100

    fixed empty lines on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import './App.css'
-import ProductCard from './components/ProductCard';
-import Header from './components/layout/Header';
-import { useSearch } from './contexts/SearchContext';
+import "./App.css";
+import ProductCard from "./components/ProductCard";
+import Header from "./components/layout/Header";
+import { useSearch } from "./contexts/SearchContext";
 
 function App() {
   const { filteredProducts, productsDummy } = useSearch();
@@ -9,34 +9,36 @@ function App() {
   return (
     <>
       <Header />
-
-
       <div className="container py-5">
         <div className="row justify-content-start g-4">
-          {filteredProducts.length != 0 ? filteredProducts.map((product) => {
-            return (
-              <div key={product.id} className="col-6 col-lg-4">
-                <ProductCard product={product} />
-              </div>
-            )
-          }) : 'Nessun prodotto trovato'}
+          {filteredProducts.length != 0
+            ? filteredProducts.map((product) => {
+                return (
+                  <div key={product.id} className="col-6 col-lg-4">
+                    <ProductCard product={product} />
+                  </div>
+                );
+              })
+            : "Nessun prodotto trovato"}
         </div>
       </div>
 
       <div className="container py-5">
         <h1>Dummy Products</h1>
         <div className="row justify-content-start g-4">
-          {productsDummy.length != 0 ? productsDummy.map((product) => {
-            return (
-              <div key={product.id} className="col-6 col-lg-4">
-                <ProductCard product={product} />
-              </div>
-            )
-          }) : 'Nessun prodotto trovato'}
+          {productsDummy.length != 0
+            ? productsDummy.map((product) => {
+                return (
+                  <div key={product.id} className="col-6 col-lg-4">
+                    <ProductCard product={product} />
+                  </div>
+                );
+              })
+            : "Nessun prodotto trovato"}
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
